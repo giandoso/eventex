f07fad12f31937c0c738565bd2aa1980b14a4358
giandoso <a15029@bcc.unifal-mg.edu.br>
2019-11-11 13:52:57 -0300

    Refatoração

---
+++ eventex/subscriptions/tests/test_mail_subscribe.py
@@ -0,0 +1,40 @@
+from django.core import mail
+from django.test import TestCase
+
+
+class SubscribePostValid(TestCase):
+    def setUp(self):
+        data = dict(name='Henrique Bastos', cpf='12345678901', email="henrique@bastos.net", phone='12345678')
+        self.resp = self.client.post('/inscricao/', data)
+        self.email = mail.outbox[0]
+
+    def test_subscription_email_subject(self):
+        expect = 'Confirmação de inscrição'
+
+        self.assertEqual(expect, self.email.subject)
+
+    def test_subscription_email_from(self):
+        expect = 'contato@eventex.com.br'
+
+        self.assertEqual(expect, self.email.from_email)
+
+    def test_subscription_email_to(self):
+        expect = ['contato@eventex.com.br', 'henrique@bastos.net']
+
+        self.assertEqual(expect, self.email.to)
+
+    def test_subscription_email_body(self):
+        contents = [
+            'Henrique Bastos',
+            '12345678901',
+            'henrique@bastos.net',
+            '12345678',
+        ]
+        for content in contents:
+            with self.subTest():
+                self.assertIn(content, self.email.body)
+
+        # self.assertIn('Henrique Bastos', self.email.body)
+        # self.assertIn('12345678901', self.email.body)
+        # self.assertIn('henrique@bastos.net', self.email.body)
+        # self.assertIn('12345678', self.email.body)
