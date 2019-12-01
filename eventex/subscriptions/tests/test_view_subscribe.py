0f52b628a9e186d163206b4ae8626f6d4ce4e8b6
giandoso <a15029@bcc.unifal-mg.edu.br>
2019-11-30 23:28:50 -0300

    Persiste a inscrição

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -1,6 +1,7 @@
 from django.core import mail
 from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 
 class SubscribeGet(TestCase):
@@ -25,8 +26,6 @@ class SubscribeGet(TestCase):
         for text, count in tags:
             with self.subTest():
                 self.assertContains(self.resp, text, count)
-
-
         # self.assertContains(self.resp, '<form')
         # self.assertContains(self.resp, '<input', 6)
         # self.assertContains(self.resp, 'type="text"', 4)
@@ -43,7 +42,6 @@ class SubscribeGet(TestCase):
         self.assertIsInstance(form, SubscriptionForm)
 
 
-
 class SubscribePostValid(TestCase):
     def setUp(self):
         data = dict(name='Henrique Bastos', cpf='12345678901', email="henrique@bastos.net", phone='12345678')
@@ -56,11 +54,12 @@ class SubscribePostValid(TestCase):
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
 
+    def test_save_subscription(self):
+        self.assertTrue(Subscription.objects.exists())
 
 class SubscribePostInvalid(TestCase):
     def setUp(self):
         self.resp = self.client.post('/inscricao/', {})
-
 
     def test_post(self):
         """Invalid Post should not redirect"""
@@ -76,6 +75,9 @@ class SubscribePostInvalid(TestCase):
     def test_form_has_errors(self):
         form = self.resp.context['form']
         self.assertTrue(form.errors)
+
+    def test_dont_save_subscriptions(self):
+        self.assertFalse(Subscription.objects.exists())
 
 
 class SubscribeSucessMessage(TestCase):
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from django.template.loader import render_to_string
 
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 
 def subscribe(request):
@@ -30,14 +31,17 @@ def create(request):
     to = form.cleaned_data['email']
     _send_mail(subject, from_, to, template_name, context)
 
+    Subscription.objects.create(**form.cleaned_data)
 
     # Success feedback
     messages.success(request, 'Inscrição realizada com sucesso!')
     return HttpResponseRedirect('/inscricao')
 
+
 def _send_mail(subject, from_, to, template_name, context):
     body = render_to_string(template_name, context)
     mail.send_mail(subject, body, from_, [from_, to])
+
 
 def new(request):
     return render(request, 'subscriptions/subscription_form.html', {'form': SubscriptionForm()})
