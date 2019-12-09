20d643ab9342b0011cc1891c929bcb3f161b9f4f
giandoso <a15029@bcc.unifal-mg.edu.br>
2019-12-09 13:22:01 -0300

    Speakers

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -3,6 +3,8 @@ from django.shortcuts import resolve_url as r
 
 
 class HomeTest(TestCase):
+    fixtures = ['keynotes.json']
+
     def setUp(self):
         self.response = self.client.get(r('home'))
 
@@ -19,8 +21,10 @@ class HomeTest(TestCase):
 
     def test_speakers(self):
         contents = [
+            'href="{}"'.format(r('speaker_detail', slug='grace-hopper')),
             'Grace Hopper',
             'http://hbn.link/hopper-pic',
+            'href="{}"'.format(r('speaker_detail', slug='alan-turing')),
             'Alan Turing',
             'http://hbn.link/turing-pic'
         ]
